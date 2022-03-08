9b61a345101314de0c9ba1374b5f90fb52c1f615
mrshuhrat <shuhratadm1n@gmail.com>
2022-03-08 10:13:53 +0500

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<>Blogs</>} />
+      <Route path="/blog/:id" element={<>Single Blog</>} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyles } from './styles/GlobalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <BrowserRouter>
+      <GlobalStyles />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
