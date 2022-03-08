9c022b211ef962462006db127688340bc4428ea5
mrshuhrat <shuhratadm1n@gmail.com>
2022-03-08 10:33:24 +0500

    feat: call single-blog page component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 //react-router-dom
 import { Route, Routes } from 'react-router-dom';
 //pages
-import { Home } from './pages';
+import { Home, SingleBlog } from './pages';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/blog/:id" element={<>Single Blog</>} />
+      <Route path="/blog/:id" element={<SingleBlog />} />
     </Routes>
   );
 }
